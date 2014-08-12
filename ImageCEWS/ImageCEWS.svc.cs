79f6073d7b26a702e6b8bf5b963e4591d142e6ea
Sven Lauterbach <svenlauterbach@live.de>
2014-08-12 16:34:58 +0200

    initial commit

---
+++ ImageCEWS/ImageCEWS.svc.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Server.Search.ContentProcessingEnrichment;
+using Microsoft.Office.Server.Search.ContentProcessingEnrichment.PropertyTypes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ImageCEWS
+{
+    /// <summary>
+    /// CEWS for extracting metadata from images.
+    /// </summary>
+    public class ImageCEWS : IContentProcessingEnrichmentService
+    {
+        private readonly ProcessedItem processedItemHolder = new ProcessedItem
+        {
+            ItemProperties = new List<AbstractProperty>()
+        };
+
+        public ProcessedItem ProcessItem(Item item)
+        {
+            if (item.RawData != null)
+            {
+                MemoryStream rawData = new MemoryStream(item.RawData);
+                BitmapMetadata metadata = GetImageMetadata(rawData);
+
+                if (!string.IsNullOrEmpty(metadata.Subject))
+                {
+                    Property<string> subject = new Property<string>();
+                    subject.Value = metadata.Subject;
+                    subject.Name = "subject";
+                    processedItemHolder.ItemProperties.Add(subject);
+                }
+
+                if (!string.IsNullOrEmpty(metadata.Comment))
+                {
+                    Property<string> comment = new Property<string>();
+                    comment.Value = metadata.Comment;
+                    comment.Name = "comment";
+                    processedItemHolder.ItemProperties.Add(comment);
+                }
+            }
+
+            return processedItemHolder;
+        }
+
+        /// <summary>
+        /// Returns metadata from an stream containing the raw image.
+        /// </summary>
+        /// <param name="image">Stream which contains the raw data of the image</param>
+        /// <returns>Metadata of image.</returns>
+        private BitmapMetadata GetImageMetadata(Stream image)
+        {
+            BitmapSource img = BitmapFrame.Create(image);
+            BitmapMetadata metadata = (BitmapMetadata)img.Metadata;
+            return metadata;
+        }
+    }
+}
---
+++ ImageCEWS/configure.ps1
@@ -0,0 +1,29 @@
+﻿#
+# Powershell script for configuring the ImageCEWS.
+#
+Add-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue
+$ssa = Get-SPEnterpriseSearchServiceApplication
+
+#
+# IMPORTANT:
+#
+# MaxRawDataSize defines the max. file size of  an image passed to the webservice.
+# If you modify this value you should also increase the value in the web.config
+# <binding maxReceivedMessageSize = "8388608">.
+#
+# The managed properties listed in "OutputProperties" should already be created 
+# before running the CEWS for the first time.
+#
+# Currently the CEWS only process jpeg files. Modify the Trigger property to
+# process other image file types. See http://msdn.microsoft.com/en-us/library/office/jj163983(v=office.15).aspx
+# for more information about the trigger expressions. 
+#
+$config = New-SPEnterpriseSearchContentEnrichmentConfiguration
+$config.Endpoint = "http://services.demo.show/ImageCEWS/ImageCEWS.svc"
+$config.InputProperties = "FileExtension"
+$config.OutputProperties = "comment", "subject"
+$config.SendRawData = $True
+$config.MaxRawDataSize = 8192 
+$config.Trigger = "FileExtension = ""jpeg"" OR FileExtension = ""jpg"""
+
+Set-SPEnterpriseSearchContentEnrichmentConfiguration –SearchApplication $ssa –ContentEnrichmentConfiguration $config
